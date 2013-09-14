6a61968b9d4ceee24c2753ba75b8242ddd656085
cponeill <caseyoneill78@hotmail.com>
2013-09-14 15:09:59 -0700

    Create addToSet.js

---
+++ addToSet.js
@@ -0,0 +1,9 @@
+function addToSet(set, values) {
+  for (var i = 0; i < values.length; i++)
+  set[value[i]] = true;
+}
+
+function removeFromSet(set, values) {
+  for (var i= 0; i < values.length; i++)
+  delete set[values[i]];
+}
