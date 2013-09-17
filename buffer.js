f08cb37d67ec6a7a2dcf5bba87ba2ff6e1159482
cponeill <caseyoneill78@hotmail.com>
2013-09-17 07:18:04 -0700

    Update buffer.js

--- buffer.js
+++ buffer.js
@@ -6,3 +6,9 @@ for (var i = 0; i < buffer.length; i++) {
   buffer[i] = i;
 }
 console.log(buffer);
+
+// Let's find the bytes between 30 and 50
+
+new bufferTwo = buffer.slice(30, 50);
+
+console.log(bufferTwo);
