95132d93175aa65405de39a291e248737457def9
cponeill <caseyoneill78@hotmail.com>
2013-09-19 10:08:41 -0700

    Update buffer.js

--- buffer.js
+++ buffer.js
@@ -12,3 +12,11 @@ console.log(buffer);
 new bufferTwo = buffer.slice(30, 50);
 
 console.log(bufferTwo);
+
+// Here is some more weird buffer code
+var buffer = new Buffer(100);
+var slice = new Buffer(40, 60);
+var targetStart = 0;
+    sourceStart = 40;
+    soureEnd = 60;
+buffer.copy(slice, targetStart, sourceStart, sourceEnd);
