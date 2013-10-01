fd1d3d6ae2ed735d854c29bc34467d2eaaccc214
cponeill <caseyoneill78@hotmail.com>
2013-09-30 21:41:27 -0700

    Update nodeRandoms.js

--- nodeRandoms.js
+++ nodeRandoms.js
@@ -162,3 +162,22 @@ exec('cat * .js wc -1', function(err, stdout, stderr) {
 	}
 	console.log(stdout);
 });
+
+// Functions and stuff
+var Line = function(x1, y1, x2, y2) {
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2;
+	this.y2 = y2;
+}
+
+Line.prototype.length = function() {
+	return Math.sqrt(
+		Math.pow(Math.abs(this.x1 - this.x2), 2) +
+		Math.pow(Math.abs(this.y1 - this.y2), 2)
+		);
+};
+
+module.exports.create = function(x1, y1, x2, y2) {
+	return Line(x1, y1, x2, y2);
+};
