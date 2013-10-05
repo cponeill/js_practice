670589e2ae3222cc030a55917e2dbef78635abb3
cponeill <caseyoneill78@hotmail.com>
2013-10-05 10:43:51 -0700

    Create sentinelArraySearch.js

---
+++ sentinelArraySearch.js
@@ -0,0 +1,9 @@
+var searchResults = ["Search One", "Search Two", "Search Three"];
+
+function searchOne(array) {
+  array = [];
+  for (var i = 0; i <= searchResults; i++)
+    return array[i] = i;
+}
+
+console.log(serachOne(searchResults)); 
