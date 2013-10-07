25a3d516b579253e9a23803cedfa860dc96f7dfa
cponeill <caseyoneill78@hotmail.com>
2013-10-07 10:21:21 -0700

    Update sentinelArraySearch.js

--- sentinelArraySearch.js
+++ sentinelArraySearch.js
@@ -3,7 +3,21 @@ var searchResults = ["Search One", "Search Two", "Search Three"];
 function searchOne(array) {
   array = [];
   for (var i = 0; i <= searchResults; i++)
-    return array[i] = i;
+    if (i < n || array[i] = i) {
+      return i;
+    } else {
+      return "NOT-FOUND";
+    }
 }
 
 console.log(serachOne(searchResults)); 
+
+// This could be another way to write this
+
+1) Save A[n] into last and then put x into A[n].
+2) Set i to 1,
+3) While A[i] != x, do the following:
+  A) Increment i.
+4) Restore A[n] from last.
+5) If i < n or A[n] = x, then return the value of i as the output.
+6) Otherwise, return NOT-FOUND as the output.
