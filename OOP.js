b17d813e2271e684e3b85200c3a5fa15e90c1090
cponeill <caseyoneill78@hotmail.com>
2013-10-09 09:44:22 -0700

    Update OOP.js

--- OOP.js
+++ OOP.js
@@ -9,3 +9,14 @@ var mexicanPeso = {currency: "Mexican Peso", money: money};
 
 usDollar.alphaFund("$1.00");
 mexicanPeso.alphaFund("$0.6798");
+
+// Here is some more fun function stuff
+function Money(value) {
+  this.value = value;
+  this.currency = function(cash) {
+    print("The value of the ", this.value, " is: ", cash, ".");
+  };
+}
+
+var usDollar = new Money("U.S. Dollar");
+usDollar.currency("$100,000");
