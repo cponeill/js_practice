446470c6884a4afbbef6b76d2d6afeeea84c0ec2
cponeill <caseyoneill78@hotmail.com>
2013-10-10 11:33:47 -0700

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -9,3 +9,12 @@ function Factorial(num) {
 
 var factorialFive = new Factorial(5);
 factorialFive.number(1);
+
+// Here is a recursive way to write this function as well
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial (n - 1);
+  }
+}
