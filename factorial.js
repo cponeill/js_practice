c97d356de13c146b50863e64d9c85bb290c2d254
cponeill <caseyoneill78@hotmail.com>
2013-10-10 10:11:38 -0700

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,2 +1,13 @@
 function Factorial(num) {
+  this.num = num;
+  this.number = function(count) {
+    if (num < count) {
+      return num * (num - count);
+    } else {
+      return num;
+    }
+  };
 }
+
+var factorialFive = new Factorial(5);
+factorialFive.number(1);
