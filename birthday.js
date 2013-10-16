ef34a6a39b2ec2a0929a37ceee31f7dd48899a85
cponeill <caseyoneill78@hotmail.com>
2013-10-16 15:29:51 -0700

    Create birthday.js

---
+++ birthday.js
@@ -0,0 +1,11 @@
+// This how I code my birthday in javscript
+
+function myBirthday(me) {
+  if (me == "enjoying my birthday") {
+    print("PARTY TIME");
+  } else {
+    print("Dude... it's your BIRTHDAY! ENJOY IT!");
+  }
+}
+
+console.log(me("enjoying my birthday"));
