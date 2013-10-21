d78c219353689389cc35b96608e62099db570673
cponeill <caseyoneill78@hotmail.com>
2013-10-21 11:11:32 -0700

    Create aspiration.js

---
+++ aspiration.js
@@ -0,0 +1,8 @@
+// This is my hairbrained idea of what the success of a startup would look like with a quick function. 
+
+function startup() {
+  while (starupCash > 0 && startupProduct == "working" && startupTeam == "action") {
+    var success == "Continue doing what you are doing no matter what it takes.";
+    startup.build(success);
+  }
+}
