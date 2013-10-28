3258076b0793a4ba0ad58770ce52180af7bb736c
cponeill <caseyoneill78@hotmail.com>
2013-10-28 12:00:48 -0700

    Create searchArray.js

---
+++ searchArray.js
@@ -0,0 +1,7 @@
+var searchArray = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing", "Sixth Thing"];
+
+for (var i = 0; i < searchArray.length; i++) {
+  ("The following is a list of things: ", searchArray[i]);
+}
+
+
