04707996bb378d6a738e628ae4043b88947bbb78
cponeill <caseyoneill78@hotmail.com>
2013-10-28 20:56:48 -0700

    Update searchArray.js

--- searchArray.js
+++ searchArray.js
@@ -1,7 +1,7 @@
 var searchArray = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing", "Sixth Thing"];
 
 for (var i = 0; i < searchArray.length; i++) {
-  ("The following is a list of things: ", searchArray[i]);
+  print("The following is a list of things: ", searchArray[i]);
 }
 
-
+print("And that was me printing a weird list of things...");
