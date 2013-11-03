5080dae5dd1fed9373474a3c4cb55a583f41a6cb
cponeill <caseyoneill78@hotmail.com>
2013-11-03 10:57:24 -0800

    Update 42.js

--- 42.js
+++ 42.js
@@ -4,12 +4,12 @@ var https = require('http');
 var fs = require('fs');
 
 var options = {
-  keys:     ('./your-key.pem'),
-  cert:     ('./your-cert-key.pem');
-}
+  keys:     fs.readFileSync('./your-key.pem'),
+  cert:     fs.readFileSync('./your-cert-key.pem');
+};
 
 var server = http.createServer(options, function(request, response) {
-  response.write(200);
+  response.writeHead(200);
   response.end('The Answer to the Ultimate Question of Life, the Universe, and Everything... is 42.');
 });
 
